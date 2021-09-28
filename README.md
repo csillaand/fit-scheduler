40b30e91ebe951cc8a715cf4f553095a55ca0d75
csilla.andras <csilla.andras@infinitpartners.com>
2021-09-28 16:06:59 +0300

    Code cleanup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BabelClient
+# Client
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.sass"],
 })
 export class AppComponent {
-  title = 'babel-client';
+    title = "client";
 
-  constructor(private translate: TranslateService) {
-    translate.setDefaultLang('ro');
-  }
+    constructor(private translate: TranslateService) {
+        translate.setDefaultLang('ro');
+    }
 }
